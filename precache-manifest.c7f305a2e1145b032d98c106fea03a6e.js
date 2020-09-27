ee750e7c933e4a5735a1525f4b050233eb529129
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-09-27 13:03:59 -0300

    presupuesto 1

---
+++ precache-manifest.c7f305a2e1145b032d98c106fea03a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42a323362aef8890c00636624bd3f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d363f04ffb96f97de2",
+    "url": "/static/css/main.96974f26.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d363f04ffb96f97de2",
+    "url": "/static/js/main.22df93d3.chunk.js"
+  },
+  {
+    "revision": "29630ad1ca7e967d236a",
+    "url": "/static/js/runtime-main.ea644902.js"
+  }
+]);
